2299037d2e6a8eb51af09935f5c4182be78f8c5a
Nicolas PUTON <n.puton@orange.fr>
2026-06-01 17:39:57 +0200

    Skeletton for the endpoints

--- server/index.js
+++ server/index.js
@@ -9,3 +9,49 @@ const port = 3001;
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
+/* ROUTES */
+// -----------------------------------
+// ----- Authentification routes -----
+
+// POST /api/sessions
+// Purpose: Authenticate the user and create a session
+app.post('/api/sessions', (req, res) => {
+  
+});
+
+// GET /api/sessions/current
+// Purpose: Check if the user is currently logged in
+app.get('/api/sessions/current', (req, res) => {
+
+});
+
+// DELETE /api/sessions/current
+// Purpose: Logout the current user and destroy the session
+app.delete('/api/sessions/current', (req, res) => {
+
+});
+
+// --------------------------------
+// ----- Game business routes -----
+
+// GET /api/game/setup
+// Purpose: Retrieve the static network map and random start/end stations
+app.get('/api/game/setup', (req, res) => {
+
+});
+
+// POST /api/game/submit
+// Purpose: Validate the route, apply events, calculate and save score
+app.post('/api/game/submit', (req, res) => {
+
+});
+
+// ------------------------------
+// ----- Leaderboard routes -----
+
+// GET /api/leaderboard
+// Purpose: Retrieve the general ranking
+app.get('/api/leaderboard', (req, res) => {
+
+});
