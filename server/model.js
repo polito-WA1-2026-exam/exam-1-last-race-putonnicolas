2d72fb2560bc07307ff993c26f8667363c5c957e
Nicolas PUTON <n.puton@orange.fr>
2026-06-01 17:59:35 +0200

    Added getMap method in dao

---
+++ server/model.js
@@ -0,0 +1,43 @@
+class User {
+  constructor(id, username, hash, salt, bestScore) {
+    this.id = id;
+    this.username = username;
+    this.hash = hash;
+    this.salt = salt;
+    this.bestScore = bestScore;
+  }
+}
+
+class Line {
+  constructor(id, name, color) {
+    this.id = id;
+    this.name = name;
+    this.color = color;
+  }
+}
+
+class Station {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+class Segment {
+  constructor(id, station1Id, station2Id, lineId) {
+    this.id = id;
+    this.station1Id = station1Id;
+    this.station2Id = station2Id;
+    this.lineId = lineId;
+  }
+}
+
+class Event {
+  constructor(id, description, effect) {
+    this.id = id;
+    this.description = description;
+    this.effect = effect;
+  }
+}
+
+export { User, Line, Station, Segment, Event };
