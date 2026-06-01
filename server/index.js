b495dea7541f40956bac5aaffc564be71fec1196
Nicolas PUTON <n.puton@orange.fr>
2026-06-01 18:16:53 +0200

    Cached the map

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 // imports
 import express from "express";
+import { getMap } from "./dao.js";
 
 // init express
 const app = new express();
 const port = 3001;
+
+// Stores the map in the server's cache since it will be the same for every player.
+const map = await getMap();
 
 // activate the server
 app.listen(port, () => {
