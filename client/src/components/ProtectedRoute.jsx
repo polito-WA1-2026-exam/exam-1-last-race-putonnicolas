367693dbbd27c94cba2c78d2bac216abb3e149f4
Nicolas PUTON <n.puton@orange.fr>
2026-06-07 15:59:19 +0200

    Added spinner

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -3,7 +3,15 @@ import { Navigate, Outlet } from "react-router"
 import UserContext from "../contexts/UserProvider.jsx"
 
 export default function ProtectedRoute() {
-  const { user } = useContext(UserContext)
+  const { user, loading } = useContext(UserContext)
+
+  if (loading) {
+    return (
+      <div className="d-flex justify-content-center align-items-center min-vh-100">
+        <div className="spinner-border text-light" role="status"></div>
+      </div>
+    )
+  }
 
   if (!user) {
     return <Navigate to="/login" replace />
