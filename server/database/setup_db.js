e885783cd02a9165882358f9814f2b21a14b4ef6
Nicolas PUTON <n.puton@orange.fr>
2026-06-09 23:03:49 +0200

    Fix station position

--- server/database/setup_db.js
+++ server/database/setup_db.js
@@ -102,12 +102,12 @@ db.serialize(() => {
   // Stations
   const insertStation = db.prepare(`INSERT INTO stations (name, x, y) VALUES (?, ?, ?)`);
   const stations = [
-      { name: 'Charpennes', x: 600, y: 150 },
-      { name: 'République', x: 520, y: 180 },
+      { name: 'Charpennes', x: 550, y: 180 },
+      { name: 'République', x: 650, y: 160 },
       { name: 'Hôtel de Ville', x: 420, y: 220 },
       { name: 'Bellecour', x: 400, y: 350 },
       { name: 'Perrache', x: 380, y: 480 },
-      { name: 'Part-Dieu', x: 650, y: 300 },
+      { name: 'Part-Dieu', x: 580, y: 300 },
       { name: 'Saxe-Gambetta', x: 550, y: 400 },
       { name: 'Jean Macé', x: 550, y: 500 },
       { name: 'Croix-Rousse', x: 400, y: 120 },
